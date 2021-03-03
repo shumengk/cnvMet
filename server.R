158e8289e3b47352a163107b901fdc9d0332ba74
shumengk <shumengk@uci.edu>
2021-03-02 23:25:40 -0800

    Revamp GUI to include slider for up/downstream analysis

--- server.R
+++ server.R
@@ -19,7 +19,7 @@ shinyServer(function(input, output) {
         
         tmp <- filter(rocPlot, gene==geneIn)
         ggplot(tmp,aes(fpr,tpr))+geom_line() + 
-            geom_segment(aes(x=0,y=0,xend = 1, yend = 1),
+            geom_segment(aes(x=0, y=0, xend = 1, yend = 1),
                          linetype = 2,col='grey')+theme_bw()+
             ggtitle(geneIn)
     })
@@ -33,7 +33,7 @@ shinyServer(function(input, output) {
         else if (input$coadOnly=="COAD tumor tissues vs. normal tissues"){
             aucValue <- coadOnlyAUC 
         }
-        gene_aucValue <- round(mean(unlist(filter(aucValue,gene==geneIn)[2:6])),5)
+        gene_aucValue <- round(mean(as.numeric(filter(aucValue,gene==geneIn)[2:6])),5)
         paste0("The AUC value of the model is: ",gene_aucValue)
     })
     
@@ -42,31 +42,55 @@ shinyServer(function(input, output) {
     # ROC plots
     output$cnvROC <- renderPlot({
         cnvInput <- input$segment
+        udInput <- input$UD
         if (cnvInput=="DMR-overlapped"){
             cnvAUCValue <- "0.59682"
             cnvROC <- cnvROCPlot 
+            ggplot(cnvROC,aes(fpr,tpr))+geom_line() + 
+                geom_segment(aes(x=0,y=0,xend = 1, yend = 1),
+                             linetype = 2,col='grey')+theme_bw()+
+                ggtitle(cnvInput) 
         }
         else {
-            cnvROC <- filter(cnvIndROC,chrLoc==cnvInput)
+            
+            lowerBound <- min(udInput[1]:udInput[2])
+            upperBound <- max(udInput[1]:udInput[2])
+            tmp <- cnvIndROC[grep(cnvInput,cnvIndROC$chrLoc),]
+            tmp1 <- filter(tmp,tmp$udLoc>=lowerBound & tmp$udLoc<=upperBound)
+            rm(tmp)
+            tmp1$udLoc <- factor(tmp1$udLoc,levels=c(-2,-1,0,1,2),
+                            labels=c("2 CNVs downstream","1 CNV downstream",
+                                    "CNV","1 CNV upstream","2 CNVs upstream"))
+            ggplot(tmp1,aes(fpr,tpr,colour=udLoc))+geom_line() + 
+                geom_segment(aes(x=0,y=0,xend = 1, yend = 1),
+                                 linetype = 2,col='grey')+theme_bw()+
+                ggtitle(cnvInput)+labs(colour="Up/downstream") 
         }
-        
-        ggplot(cnvROC,aes(fpr,tpr))+geom_line() + 
-            geom_segment(aes(x=0,y=0,xend = 1, yend = 1),
-                         linetype = 2,col='grey')+theme_bw()+
-            ggtitle(cnvInput) 
     })
-    
-    output$cnvAUC <- renderText({
+
+    output$cnvAUC <- renderTable({
         cnvInput <- input$segment
+        udInput <- input$UD
         if (cnvInput=="DMR-overlapped"){
             cnvAUCValue <- "0.59682"
+            tmp1 <- data.frame(matrix(nrow=1,ncol=2))
+            names(tmp1) <- c("Location","Mean AUC")
+            tmp1[1,] <- c(cnvInput,cnvAUCValue)
         }
         else {
-            cnvAUCValue <- round(mean(unlist(filter(cnvAUC,chrLoc==cnvInput)[2:6])),5)
+            tmp <- cnvAUC[grep(cnvInput,cnvAUC$chrLoc),]
+            lowerBound <- min(udInput[1]:udInput[2])
+            upperBound <- max(udInput[1]:udInput[2])
+            tmp1 <- filter(tmp,tmp$udLoc>=lowerBound & tmp$udLoc<=upperBound)
+
+            cnvAUCValue <- round(rowMeans(tmp1[,2:6]),5)
+            tmp1$`Mean AUC` <- cnvAUCValue
+            names(tmp1)[1] <- "Location"
         }
-        paste0("The AUC value of the CNV-based classification model is ",cnvAUCValue)
+        #paste0("The AUC value of the CNV-based classification model is ",cnvAUCValue)
+        return(tmp1[,c(1,ncol(tmp1))])
     })
-    
-   
-    
 })
+
+
+
--- ui.R
+++ ui.R
@@ -7,9 +7,9 @@ dashboardPage(
     dashboardSidebar(
         radioButtons(
             inputId="metOrCNV",label="Select the primary data source to build the classification model:",
-            choices=c("Methylation","CNV"), selected="met",width="90%"),
+            choices=c("CNV","DMR"), selected="met", width="90%"),
         conditionalPanel(
-            condition = "input.metOrCNV=='Methylation'",
+            condition = "input.metOrCNV=='DMR'",
             selectInput("metGene",
                         "Select a potential DMR-gene:",
                         choices=geneList
@@ -23,20 +23,23 @@ dashboardPage(
             condition = "input.metOrCNV=='CNV'",
             selectInput("segment",
                         "Select a CNV segment:",
-                        choices=cnvList
+                        choices=cnvList,
+                        selected=NULL
             ),
             conditionalPanel(
-                condition = "input.segment==cnvDUList[1]",
+                condition = "input.metOrCNV=='CNV'",
                 sliderInput("UD","How many CNV down/upstream?",
-                            min=-2,max=2,value=0,step=1)
+                            label = div(style='width:200px;', 
+                            div(style='float:left;', 'Downstream'), 
+                            div(style='float:right;', 'Upstream')), 
+                            min=-2,max=2,value=c(-2,2),step=1)
             )
         )
-
     ),
     
     dashboardBody(
         conditionalPanel(
-            condition = "input.metOrCNV=='Methylation'",
+            condition = "input.metOrCNV=='DMR'",
             h5("ROC Plot of Gradient Boosting Machine COAD classification models 
                on methylation data of the selected gene"),
             h6("COAD vs. Normal classifies all COAD from Normal samples."),
@@ -49,12 +52,15 @@ dashboardPage(
             condition = "input.metOrCNV=='CNV'",
             h5("ROC Plot of Gradient Boosting Machine COAD tumor tissue classification models 
                on CNV-enriched data"),
-            h6("CNV location based (chr_start_end): The CNVs were seleced through rank of importance in COAD classification model on the top 50 unmasked CNVs, 
+            h6("CNV location based (chr_start_end): The CNVs were seleced through rank of importance 
+            in COAD classification model on the 100 most common masked CNVs, 
                the methylation data at the corresponding location was extracted to build the model."),
-            h6("CNV up/downstream: The methylation data in the 1MB up/downstream region of the CNVs was used for model building."),
-            h6("DMR-overlapped: The CNVs data were first overlapped with 18 DMR genes, then the top 50 most common CNVs were selected for modeling."),
+            h6("CNV up/downstream: The methylation data at n CNV-length up/downstream regions of the CNVs 
+               was used for model building."),
+            h6("DMR-overlapped: The CNVs data was first overlapped with 18 DMR genes, then the top 
+               50 most common CNVs were selected for modeling."),
             plotOutput("cnvROC"),
-            textOutput("cnvAUC")
+            tableOutput("cnvAUC")
         ),
        
     )
